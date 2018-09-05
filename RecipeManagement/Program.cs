8bee77957ddc3094271f06a58da6b40201c0169c
Sidwell Makola <Sidwell.Makola@outlook.com>
2018-09-05 20:23:52 +0200

    Added cosmos db creation

--- RecipeManagement/Program.cs
+++ RecipeManagement/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -12,14 +14,43 @@ namespace RecipeManagement
 {
     public class Program
     {
+        private DocumentClient client;
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            try
+            {
+                BuildWebHost(args).Run();
+                var p = new Program();
+                p.Init().Wait();
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
+            }
+            catch(Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+        public async Task Init()
+        {
+            // Connect to the Azure Cosmos DB Emulator running locally
+            var p = new Program
+            {
+                client = new DocumentClient(
+                new Uri("https://localhost:8081"),
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
+            };
+
+            await p.client.CreateDatabaseIfNotExistsAsync(new Database { Id = "RecipeManagemendDb" });
+        }
     }
 }
