6b4d2736e496f0a5b8653ba5fab837cc345d5301
Sidwell Makola <Sidwell.Makola@outlook.com>
2018-09-05 21:47:27 +0200

    Fixed routing

--- RecipeManagement/ClientApp/app/components/recipe/recipe.component.spec.ts
+++ RecipeManagement/ClientApp/app/components/recipe/recipe.component.spec.ts
@@ -1,20 +1,20 @@
 /// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
 import { assert } from 'chai';
-import { CounterComponent } from './counter.component';
+import { RecipeComponent } from './recipe.component';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
-let fixture: ComponentFixture<CounterComponent>;
+let fixture: ComponentFixture<RecipeComponent>;
 
-describe('Counter component', () => {
+describe('Recipe component', () => {
     beforeEach(() => {
-        TestBed.configureTestingModule({ declarations: [CounterComponent] });
-        fixture = TestBed.createComponent(CounterComponent);
+        TestBed.configureTestingModule({ declarations: [RecipeComponent] });
+        fixture = TestBed.createComponent(RecipeComponent);
         fixture.detectChanges();
     });
 
     it('should display a title', async(() => {
         const titleText = fixture.nativeElement.querySelector('h1').textContent;
-        expect(titleText).toEqual('Counter');
+        expect(titleText).toEqual('Recipe');
     }));
 
     it('should start with count 0, then increments by 1 when clicked', async(() => {
