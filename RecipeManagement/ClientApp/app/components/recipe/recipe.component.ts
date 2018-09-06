c84741c6760870444c93aae03b49b833201d2b5c
Sidwell Makola <Sidwell.Makola@outlook.com>
2018-09-06 07:52:29 +0200

    Added Recipe listing

--- RecipeManagement/ClientApp/app/components/recipe/recipe.component.ts
+++ RecipeManagement/ClientApp/app/components/recipe/recipe.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
-    selector: 'counter',
+    selector: 'recipe',
     templateUrl: './recipe.component.html'
 })
-export class RecipeComponent {
-    public currentCount = 0;
 
-    public incrementCounter() {
-        this.currentCount++;
+export class RecipeComponent {
+    public recipes: Recipe[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/Recipe/WeatherForecasts').subscribe(result => {
+            this.recipes = result.json() as Recipe[];
+        }, error => console.error(error));
     }
+}
+
+interface Recipe {
+    title: string;
 }
